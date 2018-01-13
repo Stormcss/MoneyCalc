37bbea7ce65e1cf34b8ec9c8ece0ec33a36169b0
Strcss <stormcss@mail.ru>
2018-01-13 15:21:49 +0300

    Draft

---
+++ src/main/java/ru/strcss/projects/moneycalc/Application.java
@@ -0,0 +1,16 @@
+package ru.strcss.projects.moneycalc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@Slf4j
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        log.info("Application has started");
+
+    }
+}
